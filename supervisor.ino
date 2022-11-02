26902d887ed759b749d15b3b5c5936a3344f973b
Fernando Sanfiel Reyes <72128771+reyesanfer@users.noreply.github.com>
2022-11-02 19:09:43 +0000

    Añadidas funciones para cada opción
    Falta controlar con regex las opciones de cada orden para controlar que función se usa en los if y que opciones usan

--- supervisor.ino
+++ supervisor.ino
@@ -7,7 +7,7 @@
  * ---------------------------------------------------------------------- 
  */
 
- #define akc 1
+ #define akc 6
 
 constexpr const uint32_t serial_monitor_bauds=115200;
 constexpr const uint32_t serial1_bauds=9600;
@@ -16,7 +16,7 @@ constexpr const uint32_t pseudo_period_ms=1000;
 
 uint8_t led_state=LOW;
 
-char helpString[5] = "help";
+int option = 0;
 
 void setup()
 {
@@ -39,11 +39,50 @@ void loop()
 
   if(Serial.available()>0) { // Si se encuentra algo que leer 
     String input = Serial.readStringUntil('\n'); //Leemos hasta que se encuentra un sato de línea
+
+    // En todas las ordenes pasamos a los metodos parametros de tipo char []
+    // Ya que el metodo Serial1.write() no permite mandar tipo String
+    
     if (input == "help") {  // Si se lee help mostramos el menú de ayuda
       help();
-    } else {  // Si no mostramos que se ha leido
-      SerialUSB.print("Se ha leido: ");
-      SerialUSB.println(input);
+    } else if (input == "") { // Orden de un disparo
+      String sensor = "srf02";
+      char sens [5];
+      String option = "on";
+      char opt [8];
+      String miliseconds = "500";
+      char tmp [5];
+      sensor.toCharArray(sens, 5);
+      option.toCharArray(opt, 8);
+      miliseconds.toCharArray(tmp, 5);
+      oneshot(sens, opt, tmp);
+    } else if (input == "") { // Orden para cambiar unidades
+      String sensor = "srf02";
+      char sens [5];
+      String unidad = "cm";
+      char unit [3];
+      sensor.toCharArray(sens, 5);
+      unidad.toCharArray(unit, 3);
+      changeUnit(sens, unit);
+    } else if (input == "") { // Orden para cambiar retardo entre disparos
+      String sensor = "srf02";
+      char sens [5];
+      String miliseconds = "500";
+      char tmp [5];
+      sensor.toCharArray(sens, 5);
+      miliseconds.toCharArray(tmp, 5);
+      delayed(sens, tmp);
+    } else if (input == "") { // Orden para obtener configuración del sensor
+      String sensor = "srf02";
+      char sens [5];
+      sensor.toCharArray(sens, 5);
+      state(sens);
+    } else if (input == "us") { // Orden para mostrar lista de sensores
+      us();
+    } else {  // Si la orden introducida no es valida mostramos un mensaje por el monitor
+      SerialUSB.print("La orden ");
+      SerialUSB.print(input);
+      SerialUSB.println(" no es una orden valida. Para obtener la lista de ordenes ecriba 'help'");
     }
   }
 
@@ -56,13 +95,12 @@ void loop()
       SerialUSB.print("La medida obtenida es: ");
       SerialUSB.print(data);
       SerialUSB.println("cms");
-      Serial1.write(akc);
+      Serial1.write(akc);     // Constante definida con valor 6 para controlar que la comunicación con el sensor es correcta
       break;
     }
   }
-
-  
-  digitalWrite(LED_BUILTIN,led_state); led_state=(led_state+1)&0x01;
+  digitalWrite(LED_BUILTIN,led_state);
+  led_state=(led_state+1)&0x01;
 }
 
 
@@ -73,4 +111,82 @@ void help(){
   SerialUSB.println("- us <srf02> delay <ms>: este comando establece el tiempo de espera o retardo mínimo que debe haber entre dos disparos consecutivos del sensor (<srf02>).");
   SerialUSB.println("- us <srf02> status: este comando debe proporcionar información de configuración del sensor, en concreto, su dirección I2C, retardo mínimo entre disparos, su configuración de unidades de medida, y su estado de disparo periódico, en el caso de que éste esté activado o no.");
   SerialUSB.println("- us: este comando debe proporcionar la relación de sensores de ultrasonidos disponibles en el dispositivo sensor.");
+}
+
+// Orden de hacer oneshot, disparar continuado con 'tiempo' periodico o apagar el sensor
+int oneshot(char sensor [5], char accion [8], char tiempo [5]) {
+  option = 1;
+  Serial1.write(option);
+  Serial1.write(sensor);
+  Serial1.write(accion);
+  if (tiempo != "" || tiempo != 0) {
+    Serial1.write(tiempo);
+  }
+}
+
+// Orden para cambiar la unidad de medida del sensor
+int changeUnit(char sensor [5], char unidad [3]) {
+  option = 2;
+  Serial1.write(option);
+  Serial1.write(sensor);
+  Serial1.write(unidad);
+  uint32_t last_ms=millis();
+  while(millis()-last_ms<pseudo_period_ms) {
+    uint8_t data = Serial1.read();
+    return data;
+  }
+}
+
+// Orden para modificar el retardo entre dos disparos del sensor
+int delayed(char sensor [5], char miliseconds [5]) {
+  option = 3;
+  Serial1.write(option);
+  Serial1.write(sensor);
+  Serial1.write(miliseconds);
+  uint32_t last_ms=millis();
+  while(millis()-last_ms<pseudo_period_ms) {
+    uint8_t data = Serial1.read();
+    return data;
+  }
+}
+
+// Orden para obtener la informacion de configuración del sensor
+int state(char sensor [5]) {
+  option = 4;
+  Serial1.write(option);
+  Serial1.write(sensor);
+  uint32_t last_ms=millis();
+  while(millis()-last_ms<pseudo_period_ms) { 
+    if(Serial1.available()>0) {  
+      uint8_t data = Serial1.read();
+      SerialUSB.print("La la configuracion del sensor ");
+      SerialUSB.print(sensor);
+      SerialUSB.print(" es: ");
+      SerialUSB.println(data);
+      break;
+    }
+  }
+  last_ms=millis();
+  while(millis()-last_ms<pseudo_period_ms) {
+    uint8_t data = Serial1.read();
+    return data;
+  }
+}
+
+// Orden para informar de todos los sensores disponibles
+int us() {
+  SerialUSB.println("Los sensores disponibles son: ");
+  uint32_t last_ms=millis();
+  while(millis()-last_ms<pseudo_period_ms) { 
+    if(Serial1.available()>0) {  
+      uint8_t data = Serial1.read();
+      SerialUSB.println(data);
+      break;
+    }
+  }
+  last_ms=millis();
+  while(millis()-last_ms<pseudo_period_ms) {
+    uint8_t data = Serial1.read();
+    return data;
+  }
 }
